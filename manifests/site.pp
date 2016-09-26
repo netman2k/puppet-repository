526adc7c7160b0175834127a8308b08c8497e0bf
DaeHyung Lee <daehyung@gmail.com>
2016-09-26 08:49:29 +0000

    cleaned up

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,8 +43,4 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  #include profiles::notify_a_message
-}
-node /wordpress/{
-  include roles::wordpress
 }
